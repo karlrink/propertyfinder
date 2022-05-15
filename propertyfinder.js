bf7344fd5a98edab35204497ae7f62cf29326ac4
Karl Rink <karl@rink.us>
2022-05-15 10:53:55 -0700

    2022-05-15-0-dev-1

--- propertyfinder.js
+++ propertyfinder.js
@@ -100,6 +100,7 @@ function viewHome() {
 
         document.title = 'Home';
 
+/*
         html += `
         <div class="wrapper">
             <header class="page-header">
@@ -125,8 +126,60 @@ function viewHome() {
             </footer>
         </div>
         `;
+*/
 
-        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/search
+        html += `
+
+<nav class="menu" style="--hue: 248.9758276163942">
+  <input class="menu__toggle" id="menu-toggle" type="checkbox"/>
+  <label class="menu__toggle-label" for="menu-toggle"></label>
+  <label class="menu__toggle-label menu__toggle-label--closer" for="menu-toggle">
+    <svg class="menu__icon" preserveAspectRatio="xMinYMin" viewBox="0 0 24 24">
+      <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"></path>
+    </svg>
+    <svg class="menu__icon" preserveAspectRatio="xMinYMin" viewBox="0 0 24 24">
+      <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"></path>
+    </svg>
+  </label>
+  <ul class="menu__content">
+    <li class="menu__item" style="--x: 35; --y: 21;"><a class="menu__link" href="?">HOME</a></li>
+    <li class="menu__item" style="--x: 52; --y: 33;"><a class="menu__link" href="#ABOUT">ABOUT</a></li>
+    <li class="menu__item" style="--x: 26; --y: 61;"><a class="menu__link" href="#CONTACT">CONTACT</a></li>
+    <li class="menu__item" style="--x: 72; --y: 53;"><a class="menu__link" href="?view=maps">Maps</a></li>
+  </ul>
+</nav>
+
+<main>
+
+<!--
+  <section id="HOME">HOME</section>
+  <section id="ABOUT">ABOUT</section>
+  <section id="CONTACT">CONTACT</section>
+  <section id="WORK">WORK</section>
+-->
+
+              <label for="property-search"></label>
+              <input type="search" id="property-search" name="property-search"
+                     placeholder="Property search...">
+
+              <button type="button">Search</button>
+
+
+</main>
+
+<svg style="position: absolute; left: 100%">
+  <defs>
+    <filter id="goo">
+      <feGaussianBlur in="SourceGraphic" stdDeviation="15" result="BLUR"></feGaussianBlur>
+      <feColorMatrix in="BLUR" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="GOO"></feColorMatrix>
+      <feBlend in="SourceGraphic" in2="goo"></feBlend>
+    </filter>
+  </defs>
+</svg>
+
+        `;
+
+
 
         history.pushState({page: 'home'}, "home", "?view=home");
     }
